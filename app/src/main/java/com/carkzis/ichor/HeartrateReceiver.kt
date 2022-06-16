abcad9e31083ed2c47b7d4010cecbbe6609cb8ce
Carkzis <marc.jowett@gmail.com>
2022-06-16 17:46:24 +0100

    Add empty interfaces for sampler and heartrate receiver

---
+++ app/src/main/java/com/carkzis/ichor/HeartrateReceiver.kt
@@ -0,0 +1,3 @@
+package com.carkzis.ichor
+
+interface HeartrateReceiver {}
---
+++ app/src/main/java/com/carkzis/ichor/IchorApp.kt
@@ -0,0 +1,17 @@
+package com.carkzis.ichor
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.coroutineScope
+import timber.log.Timber
+
+@HiltAndroidApp
+class IchorApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Set up Timber for logging.
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+}
