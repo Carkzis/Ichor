426184301948122aaac4f86dbc641cdc44bc1d6a
Carkzis <marc.jowett@gmail.com>
2022-06-20 08:40:27 +0100

    Added sampler for multiple emissions and initial emission

--- app/src/main/java/com/carkzis/ichor/Sampler.kt
+++ app/src/main/java/com/carkzis/ichor/Sampler.kt
@@ -6,10 +6,19 @@ import kotlinx.coroutines.flow.flow
 class Sampler {
 
     fun sampleAtIntervals(intervalInMs: Long, initialIntervalInMs: Long = 0L) = flow {
+        checkIntervalValues(intervalInMs, initialIntervalInMs)
         delay(initialIntervalInMs)
         while (true) {
             emit(Unit)
             delay(intervalInMs)
+        }
+    }
+
+    private fun checkIntervalValues(intervalInMs: Long, initialIntervalInMs: Long = 0L) {
+        if (intervalInMs <= 0) {
+            throw IllegalArgumentException("Only zero or positive initial intervals allowed.")
+        } else if (initialIntervalInMs < 0) {
+            throw IllegalArgumentException("Only positive intervals allowed.")
         }
     }
 
--- app/src/test/java/com/carkzis/ichor/SamplerTest.kt
+++ app/src/test/java/com/carkzis/ichor/SamplerTest.kt
@@ -1,12 +1,19 @@
 package com.carkzis.ichor
 
-import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
+@ExperimentalCoroutinesApi
 internal class SamplerTest {
 
     var sut: Sampler? = null
@@ -22,7 +29,7 @@ internal class SamplerTest {
     }
 
     @Test
-    fun sampler_emitsAValue() = runBlocking {
+    fun `sampler emits a value when sampleAtIntervals is called`() = runBlocking {
         sut?.let {
             it.sampleAtIntervals(100)
                 .first()
@@ -31,6 +38,84 @@ internal class SamplerTest {
                     return@runBlocking
                 }
         }
+
         assertTrue("A value was not emitted.", false)
     }
+
+    @Test
+    fun `samples emits 10 values in 10 intervals`() = runBlocking {
+        val counter = AtomicInteger(0)
+        val intervalInMs = 100L
+        val initialIntervalInMs = 0L
+        val intervals = 10
+
+        val elapsedTimeInMs = measureTimeMillis {
+            sut?.let {
+                it.sampleAtIntervals(intervalInMs, initialIntervalInMs)
+                    .take(intervals)
+                    .collect {
+                        counter.incrementAndGet()
+                    }
+            }
+        }
+
+        assertCorrectAmountOfEmissions(intervals, counter.get())
+        assertCorrectAmountOfTimeElapsed(elapsedTimeInMs, intervals, intervalInMs, initialIntervalInMs)
+    }
+
+    @Test
+    fun `samples emit 1 value after initial interval of 1 seconds`() = runBlocking {
+        val counter = AtomicInteger(0)
+        val intervalInMs = 150L
+        val initialIntervalInMs = 1000L
+        val intervals = 1
+
+        val elapsedTimeInMs = measureTimeMillis {
+            sut?.let {
+                it.sampleAtIntervals(intervalInMs, initialIntervalInMs)
+                    .take(intervals)
+                    .collect {
+                        counter.incrementAndGet()
+                    }
+            }
+        }
+
+        assertCorrectAmountOfEmissions(intervals, counter.get())
+        assertCorrectAmountOfTimeElapsed(elapsedTimeInMs, intervals, intervalInMs, initialIntervalInMs)
+    }
+
+    // TODO: Negative intervals.
+
+    private fun assertCorrectAmountOfTimeElapsed(
+        elapsedTimeInMs: Long,
+        intervals: Int,
+        intervalInMs: Long,
+        initialIntervalInMs: Long
+    ) {
+
+        val expectedLowerBoundInMs =
+            getExpectedLowerBoundsInMs(intervals, intervalInMs, initialIntervalInMs)
+        val expectedUpperBoundInMs = getExpectedUpperBoundsInMs(expectedLowerBoundInMs)
+
+        assertTrue(
+            "Elapsed time was $elapsedTimeInMs, lower bound was $expectedLowerBoundInMs, " +
+                    "upper bound was $expectedUpperBoundInMs",
+            elapsedTimeInMs in expectedLowerBoundInMs..expectedUpperBoundInMs
+        )
+    }
+
+    private fun getExpectedLowerBoundsInMs(
+        intervals: Int,
+        intervalInMs: Long,
+        initialIntervalInMs: Long
+    ) =
+        ((intervals - 1) * intervalInMs) + initialIntervalInMs
+
+    private fun getExpectedUpperBoundsInMs(expectedLowerBounds: Long, multiplier: Double = 1.2) =
+        (expectedLowerBounds * multiplier).toInt()
+
+    private fun assertCorrectAmountOfEmissions(expectedEmissions: Int, actualEmissions: Int) {
+        assertTrue("Value was $actualEmissions", expectedEmissions == actualEmissions)
+    }
+
 }
