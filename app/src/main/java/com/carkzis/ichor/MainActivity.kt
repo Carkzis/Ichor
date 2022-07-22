d1e14be77a7d24e6371d6cac624e463737fa1a45
Carkzis <marc.jowett@gmail.com>
2022-07-22 12:37:29 +0100

    Allow permission for heart rate monitoring using button

--- app/src/main/java/com/carkzis/ichor/MainActivity.kt
+++ app/src/main/java/com/carkzis/ichor/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.carkzis.ichor
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,9 +8,11 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewModelScope
@@ -17,8 +20,11 @@ import com.carkzis.ichor.theme.IchorTheme
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.AutoCenteringParams
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.Text
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.rememberPermissionState
 import timber.log.Timber
 
@@ -37,27 +43,39 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun IchorUI(modifier: Modifier = Modifier, viewModel: MainViewModel = viewModel()) {
     val heartRatePermission = rememberPermissionState(android.Manifest.permission.BODY_SENSORS)
+    // Note: Reset permissions on an emulator using the command "adb shell pm reset-permissions".
     when (heartRatePermission.hasPermission) {
         true -> {
             viewModel.initiateDataCollection()
             val heartState by viewModel.latestHeartRate.collectAsState()
-            ScalingLazyColumn(modifier = Modifier.fillMaxWidth(), autoCentering = AutoCenteringParams(itemIndex = 0)) {
+            ScalingLazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
+                autoCentering = AutoCenteringParams(itemIndex = 0)
+            ) {
                 item { IchorText(stringResourceId = R.string.hello_world) }
                 item { IchorStatefulText(text = heartState.toString()) }
+                item { Button(onClick = { heartRatePermission.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }}
             }
         }
         false -> {
-            ScalingLazyColumn(modifier = Modifier.fillMaxWidth(), autoCentering = AutoCenteringParams(itemIndex = 0)) {
+            ScalingLazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
+                autoCentering = AutoCenteringParams(itemIndex = 0)
+            ) {
                 item { IchorText(stringResourceId = R.string.hello_world) }
+                item { Button(onClick = { heartRatePermission.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }}
             }
         }
     }
