73dbd5ad290d31e4ed56574dc758904b76a804c8
Carkzis <marc.jowett@gmail.com>
2022-09-02 13:58:51 +0100

    Add adapter function from database entity to domain data class

--- app/src/main/java/com/carkzis/ichor/DatabaseEntities.kt
+++ app/src/main/java/com/carkzis/ichor/DatabaseEntities.kt
@@ -2,6 +2,7 @@ package com.carkzis.ichor
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import timber.log.Timber
 import java.time.LocalDateTime
 import java.util.*
 
@@ -13,8 +14,23 @@ data class LocalHeartRate(
     val value: String
 )
 
+data class DomainHeartRate(
+    val date: String,
+    val value: Double
+)
+
 fun HeartRateDataPoint.toLocalHeartRate() = LocalHeartRate(
     pk = UUID.randomUUID().toString(),
     date = LocalDateTime.now().toString(),
     value = this.value.asDouble().toString()
+)
+
+fun LocalHeartRate.toDomainHeartRate() = DomainHeartRate(
+    date = this.date,
+    value = try {
+        this.value.toDouble()
+    } catch (e: Exception) {
+        Timber.e(e.localizedMessage)
+        0.0
+    }
 )
