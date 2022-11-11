730ac68f3b765e02ce1628aac7b2f62548c85334
Carkzis <marc.jowett@gmail.com>
2022-11-11 16:47:49 +0000

    Extract main UI items into own function

--- app/src/main/java/com/carkzis/ichor/MainActivity.kt
+++ app/src/main/java/com/carkzis/ichor/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterialApi::class, ExperimentalPermissionsApi::class)
+
 package com.carkzis.ichor
 
 import android.Manifest
@@ -5,38 +7,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MonitorHeart
-import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.health.services.client.data.Availability
-import androidx.health.services.client.data.DataTypeAvailability
-import androidx.health.services.client.proto.DataProto
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.*
-import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.Text
 import com.carkzis.ichor.theme.IchorColorPalette
 import com.carkzis.ichor.theme.IchorTheme
 import com.carkzis.ichor.theme.IchorTypography
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.rememberPermissionState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.StateFlow
-import timber.log.Timber
-import java.sql.Time
 import java.util.concurrent.atomic.AtomicBoolean
 
 @AndroidEntryPoint
@@ -55,81 +51,110 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun IchorUI(modifier: Modifier = Modifier, viewModel: MainViewModel) {
-    val heartRatePermission = rememberPermissionState(Manifest.permission.BODY_SENSORS)
     // Note: Reset permissions on an emulator using the command "adb shell pm reset-permissions".
+
     // TODO: DELETE ITEM functionality (SwipeToDismiss).
     // TODO: DELETE ALL functionality (with dialog?)
     // TODO: Change sampling time and show current sampling time (in settings view/dialog?)
+
+    val heartRatePermission = rememberPermissionState(Manifest.permission.BODY_SENSORS)
     val listState = rememberScalingLazyListState()
+    val heartRates by viewModel.latestHeartRateList.collectAsState()
+    val shouldInitiateDataCollection by remember { mutableStateOf(AtomicBoolean(true)) }
 
     Scaffold(
         timeText = { if (!listState.isScrollInProgress) TimeText() },
         vignette = { Vignette(vignettePosition = VignettePosition.TopAndBottom) },
         positionIndicator = { PositionIndicator(scalingLazyListState = listState) }
     ) {
-        val heartRates by viewModel.latestHeartRateList.collectAsState()
-        val shouldInitiateDataCollection by remember { mutableStateOf(AtomicBoolean(true)) }
+        DisplayUIItems(
+            modifier,
+            listState,
+            viewModel,
+            heartRatePermission,
+            shouldInitiateDataCollection,
+            heartRates
+        )
+    }
+}
 
-        ScalingLazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier.fillMaxWidth(),
-            autoCentering = AutoCenteringParams(itemIndex = 0),
-            state = listState
-        ) {
-            item { DisplayMainIcon() }
-            item { DisplayTitle(modifier = modifier) }
+@Composable
+private fun DisplayUIItems(
+    modifier: Modifier,
+    listState: ScalingLazyListState,
+    viewModel: MainViewModel,
+    heartRatePermission: PermissionState,
+    shouldInitiateDataCollection: AtomicBoolean,
+    heartRates: List<DomainHeartRate>
+) {
+    ScalingLazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth(),
+        autoCentering = AutoCenteringParams(itemIndex = 0),
+        state = listState
+    ) {
+        item { DisplayMainIcon() }
+        item { DisplayTitle(modifier = modifier) }
+        item {
+            DisplayAvailability(modifier = modifier, state = viewModel.latestAvailability)
+        }
+        if (heartRatePermission.hasPermission) {
+            initiateDataCollectionOnce(shouldInitiateDataCollection, viewModel)
             item {
-                DisplayAvailability(modifier = modifier, state = viewModel.latestAvailability)
+                DisplayLatestHeartRate(modifier = modifier, state = viewModel.latestHeartRate)
             }
-
-            // HEARTRATES
-            if (heartRatePermission.hasPermission) {
-                initiateDataCollectionOnce(shouldInitiateDataCollection, viewModel)
-                item {
-                    DisplayLatestHeartRate(modifier = modifier, state = viewModel.latestHeartRate)
-                }
-                items(
-                    items = heartRates,
-                    key = { it.pk }
-                ) { currentHeartRateData ->
-                    val dismissState = rememberDismissState {
-                        if (it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart) {
-                            viewModel.deleteHeartRate(currentHeartRateData.pk)
-                        }
-                        true
-                    }
-
-                    SwipeToDismiss(
-                        state = dismissState,
-                        background = { Box(modifier = modifier.fillMaxSize()) },
-                        dismissContent = {
-                            IchorCard(
-                                time = currentHeartRateData.date,
-                                content = {
-                                    Text(
-                                        "${currentHeartRateData.value} bpm",
-                                        color = IchorColorPalette.onSecondary
-                                    )
-                                }
-                            )
-                        })
-                }
-            } else if (!heartRatePermission.permissionRequested) {
-                item { IchorButton(onClick = { heartRatePermission.launchPermissionRequest() }) }
-            } else {
-                item {
-                    IchorText(
-                        modifier = modifier,
-                        style = IchorTypography.body2,
-                        stringResourceId = R.string.app_permission_was_denied
-                    )
-                }
+            items(
+                items = heartRates,
+                key = { it.pk }
+            ) { currentHeartRateData ->
+                DisplayListOfHeartRates(viewModel, currentHeartRateData, modifier)
             }
+        } else if (!heartRatePermission.permissionRequested) {
+            item { IchorButton(onClick = { heartRatePermission.launchPermissionRequest() }) }
+        } else {
+            item { DisplayPermissionsInstructions(modifier) }
         }
     }
+}
+
+@Composable
+private fun DisplayPermissionsInstructions(modifier: Modifier) {
+    IchorText(
+        modifier = modifier,
+        style = IchorTypography.body2,
+        stringResourceId = R.string.app_permission_was_denied
+    )
+}
+
+@Composable
+private fun DisplayListOfHeartRates(
+    viewModel: MainViewModel,
+    currentHeartRateData: DomainHeartRate,
+    modifier: Modifier
+) {
+    val dismissState = rememberDismissState {
+        if (it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart) {
+            viewModel.deleteHeartRate(currentHeartRateData.pk)
+        }
+        true
+    }
+    SwipeToDismiss(
+        state = dismissState,
+        background = { Box(modifier = modifier.fillMaxSize()) },
+        dismissContent = {
+            IchorCard(
+                time = currentHeartRateData.date,
+                content = {
+                    Text(
+                        "${currentHeartRateData.value} bpm",
+                        color = IchorColorPalette.onSecondary
+                    )
+                }
+            )
+        })
 }
 
 private fun initiateDataCollectionOnce(
