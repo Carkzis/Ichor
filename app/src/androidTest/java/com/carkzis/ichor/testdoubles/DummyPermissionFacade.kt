2db026321734f0dc9ef8181fdcc956052c6bd3ef
Carkzis <marc.jowett@gmail.com>
2022-12-23 11:33:19 +0000

    Add test for denying permissions having not previously denied them

--- app/src/androidTest/java/com/carkzis/ichor/testdoubles/DummyPermissionFacade.kt
+++ app/src/androidTest/java/com/carkzis/ichor/testdoubles/DummyPermissionFacade.kt
@@ -3,11 +3,9 @@ package com.carkzis.ichor.testdoubles
 import com.carkzis.ichor.utils.PermissionFacade
 import kotlinx.coroutines.flow.MutableStateFlow
 
-object DummyPermissionFacade: PermissionFacade {
-    var willGivePermission = false
-    var permissionPreviouslyDenied = false
-    var dummyHasPermission = MutableStateFlow(false)
-    var dummyPermissionRequested = MutableStateFlow(permissionPreviouslyDenied)
+class DummyPermissionFacade(private var willGivePermission: Boolean = false, private var permissionPreviouslyDenied: Boolean = false): PermissionFacade {
+    private var dummyHasPermission = MutableStateFlow(false)
+    private var dummyPermissionRequested = MutableStateFlow(permissionPreviouslyDenied)
 
     override fun getPermission(): MutableStateFlow<Boolean> = dummyHasPermission
     override fun getPermissionRequested(): MutableStateFlow<Boolean> = dummyPermissionRequested
--- app/src/androidTest/java/com/carkzis/ichor/tests/ui/IchorScreenPermissionRequiredTest.kt
+++ app/src/androidTest/java/com/carkzis/ichor/tests/ui/IchorScreenPermissionRequiredTest.kt
@@ -1,19 +1,28 @@
 package com.carkzis.ichor.tests.ui
 
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.NavHostController
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 import com.carkzis.ichor.IchorNavHost
 import com.carkzis.ichor.IchorScreens
+import com.carkzis.ichor.R
 import com.carkzis.ichor.testdoubles.DummyPermissionFacade
 import com.carkzis.ichor.testdoubles.DummyViewModel
 import com.carkzis.ichor.ui.IchorScreen
 import com.carkzis.ichor.utils.DefaultPermissionFacade
 import com.carkzis.ichor.utils.PermissionFacade
 import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
 class IchorScreenPermissionRequiredTest {
 
     @get:Rule(order = 0)
@@ -21,19 +30,60 @@ class IchorScreenPermissionRequiredTest {
 
     @get:Rule(order = 1)
     val composeTestRule = createComposeRule()
-    val heartRatePermissionFacade: PermissionFacade = DummyPermissionFacade
+    lateinit var heartRatePermissionFacade: DummyPermissionFacade
     lateinit var navController: NavHostController
+    lateinit var delayedSetUp: () -> Unit
+    lateinit var permissionNotGrantedText: String
 
     @Before
     fun setUp() {
-        hiltRule.inject()
-        composeTestRule.apply {
-            setContent {
-                navController = rememberSwipeDismissableNavController()
-                IchorScreen(viewModel = DummyViewModel(), heartRatePermissionFacade = heartRatePermissionFacade)
-//                IchorNavHost(viewModel = DummyViewModel(), navHostController = navController, startDestination = IchorScreens.ICHOR.toString())
+        delayedSetUp = {
+            hiltRule.inject()
+            composeTestRule.apply {
+                setContent {
+                    navController = rememberSwipeDismissableNavController()
+                    permissionNotGrantedText = stringResource(id = R.string.app_permission_was_denied)
+                    IchorScreen(viewModel = DummyViewModel(), heartRatePermissionFacade = heartRatePermissionFacade)
+                }
             }
         }
     }
+
+    @Test
+    fun `when no permission previously requested can get permission dialogue which after denial message provided on screen`() {
+        heartRatePermissionFacade = DummyPermissionFacade(willGivePermission = false, permissionPreviouslyDenied = false)
+        delayedSetUp()
+
+        // Assert on the initial screen.
+        composeTestRule
+            .onNodeWithContentDescription("Permission request button")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithContentDescription("Permission request button")
+            .performClick()
+
+        // Assert the new screen after "denying" permissions.
+        composeTestRule
+            .onAllNodesWithContentDescription("Permission request button")
+            .assertCountEquals(0)
+        composeTestRule
+            .onNodeWithText(permissionNotGrantedText)
+            .assertIsDisplayed()
+    }
+//
+//    @Test
+//    fun `when no permission previously requested, can get permission dialogue which after acceptance, heart rate data screen visible`() {
+//
+//    }
+//
+//    @Test
+//    fun `when permission previously granted, automatically go to heart rate data screen visible`() {
+//
+//    }
+//
+//    @Test
+//    fun `when permission previously denied, message provided on screen and permission cannot be requested again within app`() {
+//
+//    }
     
 }
