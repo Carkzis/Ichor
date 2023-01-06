5bd88dac8475cf95e38e93188f610c87809d5c83
Carkzis <marc.jowett@gmail.com>
2023-01-06 15:56:46 +0000

    Add text resource strings to AboutTest

--- app/src/androidTest/java/com/carkzis/ichor/tests/ui/AboutScreenTest.kt
+++ app/src/androidTest/java/com/carkzis/ichor/tests/ui/AboutScreenTest.kt
@@ -3,6 +3,7 @@ package com.carkzis.ichor.tests.ui
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.health.services.client.data.Availability
 import androidx.navigation.NavHostController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
@@ -31,6 +32,26 @@ class AboutScreenTest {
     private lateinit var aboutDescription: String
     private lateinit var startingUp: String
     private lateinit var permissions: String
+    private lateinit var aboutAbout: String
+    private lateinit var whatYouCanSee: String
+    private lateinit var availabilitySubtitle: String
+    private lateinit var availability: String
+    private lateinit var samplingSpeedSubtitle: String
+    private lateinit var samplingSpeedDisplay: String
+    private lateinit var bpmSubtitle: String
+    private lateinit var bpm: String
+    private lateinit var historySubtitle: String
+    private lateinit var history: String
+    private lateinit var whatYouCanDo: String
+    private lateinit var samplingSpeed: String
+    private lateinit var slowSampling: String
+    private lateinit var defaultSampling: String
+    private lateinit var fastSampling: String
+    private lateinit var deleteAll: String
+    private lateinit var deleteOneSubtitle: String
+    private lateinit var deleteOne: String
+    private lateinit var furtherInformation: String
+    private lateinit var furtherInformationDetails: String
 
     @Before
     fun setUp() {
@@ -41,6 +62,27 @@ class AboutScreenTest {
                 aboutDescription = stringResource(id = R.string.about_description)
                 startingUp = stringResource(id = R.string.about_starting_up)
                 permissions = stringResource(id = R.string.about_permissions)
+                aboutAbout = stringResource(id = R.string.about_about)
+                whatYouCanSee = stringResource(id = R.string.about_what_you_can_see)
+                availabilitySubtitle = stringResource(id = R.string.about_availability_subtitle)
+                availability = stringResource(id = R.string.about_availability)
+                samplingSpeedSubtitle = stringResource(id = R.string.about_sampling_speed_subtitle)
+                samplingSpeedDisplay = stringResource(id = R.string.about_sampling_speed_display)
+                bpmSubtitle = stringResource(id = R.string.about_bpm_subtitle)
+                bpm = stringResource(id = R.string.about_bpm)
+                historySubtitle = stringResource(id = R.string.about_history_subtitle)
+                history = stringResource(id = R.string.about_history)
+                whatYouCanDo = stringResource(id = R.string.about_what_you_can_do)
+                samplingSpeed = stringResource(id = R.string.about_sampling_speed)
+                slowSampling = stringResource(id = R.string.about_slow_sampling)
+                defaultSampling = stringResource(id = R.string.about_default_sampling)
+                fastSampling = stringResource(id = R.string.about_fast_sampling)
+                deleteAll = stringResource(id = R.string.about_delete_all)
+                deleteOneSubtitle = stringResource(id = R.string.about_delete_one_subtitle)
+                deleteOne = stringResource(id = R.string.about_delete_one)
+                furtherInformation = stringResource(id = R.string.about_further_information)
+                furtherInformationDetails = stringResource(id = R.string.about_further_information_details)
+
                 navController = rememberSwipeDismissableNavController()
                 AboutScreen()
             }
@@ -48,7 +90,7 @@ class AboutScreenTest {
     }
 
     @Test
-    fun `about screen items are displayed as expected`() {
+    fun `about screen text items are displayed as expected`() {
         headerDisplayed()
 
         composeTestRule
